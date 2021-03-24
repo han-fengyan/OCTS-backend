a73ab21bc98fea2bd99acabcffecd07dcffd212d
韩奉延 <2019011242@secoder.net>
2021-03-24 21:34:19 +0800

    bug fix: fix urls.py views.py

--- octs/urls.py
+++ octs/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.main_view),
-    path('signup/',views.signup, name='signup')
+    path('signup/',views.signup, name='signup'),
     path('login/', views.login, name='login'),
 ]
