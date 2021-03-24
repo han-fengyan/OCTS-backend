6e9c4e10b96c1daac6786e551702a3f0afee341a
韩奉延 <2019011242@secoder.net>
2021-03-24 21:44:57 +0800

    bug fix: modify tests.py

--- octs/tests.py
+++ octs/tests.py
@@ -1,12 +1,11 @@
 from django.test import TestCase
-from .models import User
+
 # Create your tests here.
 
 
 class MyTest(TestCase):
     def setUp(self) -> None:
         self.data = 1
-        alice = User.objects.create(name="Alice",password = "123456")
-        bob = User.objects.create(name = "Bob", password = "123456")
-    def test_add_new_user(self):
-        pass
+
+    def test_fslls(self):
+        self.assertTrue(self.data == 1)
