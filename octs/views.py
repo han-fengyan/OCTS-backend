427d40b09a1d00b3f220c819a49e482048035466
韩奉延 <2019011242@secoder.net>
2021-03-25 09:37:59 +0800

    feat:add corsheaders #14

--- octs/views.py
+++ octs/views.py
@@ -21,22 +21,22 @@ def signup(request):
         request_data =request.body
         json_str = request_data.decode()
 
-    #判断前端发来的数据是否合法
-    try:
-        json_data = json.loads(json_str)
-        username = json_data['username']
-        password = json_data['password']
-    except Exception as e:
-        return gen_response(400, "message is invalid")
+        #判断前端发来的数据是否合法
+        try:
+            json_data = json.loads(json_str)
+            username = json_data['username']
+            password = json_data['password']
+        except Exception as e:
+            return gen_response(400, "message is invalid")
 
-        #需要判断是否已经有重复用户名，并将查询结果返回前端
-    inuser = User.objects.filter(user=username)
-    if inuser:
-        return gen_response(406,'user has existed')
-    else:
-        user = User(name = username, password = password)
-        user.save()
-        return gen_response(201,"sign up successfully")
+            #需要判断是否已经有重复用户名，并将查询结果返回前端
+        inuser = User.objects.filter(user=username)
+        if inuser:
+            return gen_response(406,'user has existed')
+        else:
+            user = User(name = username, password = password)
+            user.save()
+            return gen_response(201,"sign up successfully")
 
 
 def login(request):
