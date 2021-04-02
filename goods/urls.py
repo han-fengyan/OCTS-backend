6d0773f6369b620cd20663ea32c22839559c37b5
高敬越 <2019011230@secoder.net>
2021-04-02 23:21:20 +0800

    feat(goods): add picture to product
    add new modle of picture with link to product,
    & created new method handles htmlform
    #21

--- goods/urls.py
+++ goods/urls.py
@@ -3,10 +3,10 @@ from django.views.decorators.csrf import csrf_exempt
 from . import views
 
 urlpatterns = [
-    path('upload/', csrf_exempt(views.add)),
+    path('upload/', csrf_exempt(views.add_product)),
     path('list/', views.products_list),
     path('merchant/', views.all_products),
     path('details/<int:id>', views.detail),
-    path('modify/<int:id>', csrf_exempt(views.modify)),
-    path('offshelf/', csrf_exempt(views.remove)),
+    path('modify/<int:id>', views.modify),
+    path('status/', views.on_off_shelf),
 ]
