18077795d2abe20696ae0df9b57f990051eec3dd
韩奉延 <2019011242@secoder.net>
2021-04-06 22:52:33 +0800

    fix : register Coupon in admin

--- octs/admin.py
+++ octs/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 import sys
-from .models import User
+from .models import User, Coupon
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Coupon)
